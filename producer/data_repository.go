528b6aa717f38492bd785610d294c3ef00258c50
AshithaCDAC <ashitha@cdac.in>
2026-04-06 14:39:03 +0000

    sm data retrieval fix

--- producer/data_repository.go
+++ producer/data_repository.go
@@ -246,7 +246,7 @@ func HandleCreateAmfContext3gpp(request *httpwrapper.Request) *httpwrapper.Respo
 	ueId := request.Params["ueId"]
 	collName := SUBSCDATA_CTXDATA_AMF_3GPPACCESS
 
-	problemDetails, ok, exists, err := CreateAmfContext3gppProcedure(collName, ueId, Amf3GppAccessRegistration)
+	problemDetails, exists, err := CreateAmfContext3gppProcedure(collName, ueId, Amf3GppAccessRegistration)
 	if err != nil {
 		stats.IncrementUdrSubscriptionDataStats("create", "amf-3gpp-access", "FAILURE")
 		return httpwrapper.NewResponse(http.StatusForbidden, nil, problemDetails)
@@ -254,16 +254,16 @@ func HandleCreateAmfContext3gpp(request *httpwrapper.Request) *httpwrapper.Respo
 	if exists {
 		return httpwrapper.NewResponse(http.StatusNoContent, nil, map[string]interface{}{})
 	}
-	if !ok {
-		logger.DataRepoLog.Debugln("PUT request failed")
-	}
+	// if !ok {
+	// 	logger.DataRepoLog.Debugln("PUT request failed")
+	// }
 	stats.IncrementUdrSubscriptionDataStats("create", "amf-3gpp-access", "SUCCESS")
 	return httpwrapper.NewResponse(http.StatusCreated, nil, map[string]interface{}{})
 }
 
 func CreateAmfContext3gppProcedure(collName string, ueId string,
 	Amf3GppAccessRegistration models.Amf3GppAccessRegistration,
-) (*models.ProblemDetails, bool, bool, error) {
+) (*models.ProblemDetails, bool, error) {
 	var exists bool
 	colleName := SUBSCDATA_AUTHDATA_AUTHSTATUS
 	filters := bson.M{"ueId": ueId}
@@ -276,7 +276,7 @@ func CreateAmfContext3gppProcedure(collName string, ueId string,
 		logger.DataRepoLog.Debugln("ueId found from mongodb")
 	} else {
 		logger.DataRepoLog.Debugln("ueId not found from mongodb")
-		return util.ProblemDetailsNotFound("SUBSCRIPTION_NOT_FOUND"), false, false, errors.New("no required subscription data")
+		return util.ProblemDetailsNotFound("SUBSCRIPTION_NOT_FOUND"), false, errors.New("no required subscription data")
 	}
 
 	filter := bson.M{"ueId": ueId}
@@ -290,13 +290,15 @@ func CreateAmfContext3gppProcedure(collName string, ueId string,
 	if data != nil {
 		logger.DataRepoLog.Debugln("ueId exist")
 		exists = true
+	} else {
+		logger.DataRepoLog.Debugln("---ueId not exist")
 	}
 
-	ok, errPutOne := CommonDBClient.RestfulAPIPutOne(collName, filter, putData)
+	_, errPutOne := CommonDBClient.RestfulAPIPutOne(collName, filter, putData)
 	if errPutOne != nil {
 		logger.DataRepoLog.Warnln(errPutOne)
 	}
-	return nil, ok, exists, errPutOne
+	return nil, exists, errPutOne
 }
 
 func HandleQueryAmfContext3gpp(request *httpwrapper.Request) *httpwrapper.Response {
@@ -3137,6 +3139,19 @@ func QuerySmDataProcedure(collName string, ueId string, servingPlmnId string,
 		for key, value := range doc {
 			if value == nil {
 				delete(doc, key)
+			}
+		}
+
+		if dnnConfigs, ok := doc["dnnConfigurations"].(map[string]interface{}); ok {
+			for dnn, cfg := range dnnConfigs {
+				if cfgMap, ok := cfg.(map[string]interface{}); ok {
+					for k, v := range cfgMap {
+						if v == nil {
+							delete(cfgMap, k)
+						}
+					}
+					dnnConfigs[dnn] = cfgMap
+				}
 			}
 		}
 	}
